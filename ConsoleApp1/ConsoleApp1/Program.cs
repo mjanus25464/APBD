0661e386b59e503f3c5805c7995852f5d50504ff
s25464 <s25464@pjwstk.edu.pl>
2024-03-18 05:20:12 +0100

    Powrot do default main

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,16 +2,6 @@
 {
     public static void Main(string[] args)
     {
-        int[] data = {34,12,35,62,32,12,65,15,97,46,74};
-        ArytmeticAverage(data);
     }
 
-    public static void ArytmeticAverage(int[] data)
-    {
-        int avr = 0;
-        foreach (int number in data)
-            avr += number;
-        avr /= data.Length;
-        Console.WriteLine("Average value: " + avr);
-    }
 }
